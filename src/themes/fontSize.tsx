90914748265a895f626d569caec35c5da777c9da
Aldi-H <aldihimawan88@gmail.com>
2023-08-20 20:39:33 +0700

    style: change size using react-native-responsive-screen

--- src/themes/fontSize.tsx
+++ src/themes/fontSize.tsx
@@ -1,6 +1,17 @@
+import {
+  // widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
 export const fontSizes = {
+  heading1: hp('3.5%'),
+  heading2: hp('2.3%'),
+  body: hp('2%'),
+  caption: hp('1.5%'),
+};
+/* export const fontSizes = {
   heading1: 20,
   heading2: 16,
   body: 12,
   caption: 10,
-};
+}; */
