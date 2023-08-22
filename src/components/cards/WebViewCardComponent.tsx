b14a817a63e4247d58e1fdc7965e6308b50fa417
Aldi-H <aldihimawan88@gmail.com>
2023-08-22 20:29:53 +0700

    feat: add card for webView

---
+++ src/components/cards/WebViewCardComponent.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {
+  // widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import WebViewImage1 from '../../assets/images/WebViewImage1.svg';
+import { CONSTANT } from '../../themes';
+
+const WebViewCardComponent = () => {
+  return (
+    <TouchableOpacity
+      style={styles.WebViewCardContainer}
+      className="flex rounded-md shadow-md shadow-gray-600"
+      onPress={() => {
+        console.log('WebView Card Pressed');
+      }}>
+      <View className="absolute self-center inset-0 object-cover z-0 my-16">
+        <WebViewImage1 height={hp('13%')} />
+      </View>
+      <View className="relative z-10 mt-4 mx-6 items-center">
+        {/* Change This Text later */}
+        <Text style={styles.WebViewContentTitle} className="text-center ">
+          Ketahui Parameter Kondisi Kolam Udang yang Baik dalam Mengelola Kolam
+          Udang
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  WebViewCardContainer: {
+    backgroundColor: CONSTANT.themeColors.base,
+    height: hp('22%'),
+  },
+  imageContainer: {
+    height: hp('1%'),
+  },
+  WebViewContentTitle: {
+    fontFamily: CONSTANT.customFonts.heading2,
+    fontSize: CONSTANT.fontSizes.body,
+    color: CONSTANT.themeColors.font,
+  },
+});
+
+export default WebViewCardComponent;
