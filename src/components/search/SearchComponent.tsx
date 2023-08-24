c3d8f18049b4691514a5ff2eb8f7839728cc4557
Aldi-H <aldihimawan88@gmail.com>
2023-08-24 19:23:08 +0700

    feat: add search bar

---
+++ src/components/search/SearchComponent.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TextInput, StyleSheet, View } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/solid';
+
+import { CONSTANT } from '../../themes';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const SearchComponent = () => {
+  return (
+    <View
+      style={styles.searchContainer}
+      className="flex flex-row items-center w-full rounded-md text-gray-500">
+      <TouchableOpacity className="ml-2">
+        <MagnifyingGlassIcon strokeWidth={2} fill="black" size={hp('3.5%')} />
+      </TouchableOpacity>
+      <View className="mx-2">
+        <TextInput style={styles.searchPlaceholder} placeholder="Cari Disini" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    backgroundColor: CONSTANT.themeColors.base,
+    height: hp('5.5%'),
+  },
+  searchPlaceholder: {
+    fontFamily: CONSTANT.customFonts.body,
+    fontSize: CONSTANT.fontSizes.body,
+    color: CONSTANT.themeColors.disable,
+    width: wp('70%'),
+  },
+});
+
+export default SearchComponent;
