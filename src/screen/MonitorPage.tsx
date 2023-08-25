0b1c4c0c90f067fcfecb4e93dba5f60f540f7694
Aldi-H <aldihimawan88@gmail.com>
2023-08-25 19:59:19 +0700

    chore: show list of item

--- src/screen/MonitorPage.tsx
+++ src/screen/MonitorPage.tsx
@@ -5,11 +5,13 @@ import {
   StatusBar,
   SafeAreaView,
   FlatList,
+  Text,
 } from 'react-native';
 import {
   // widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 // import { useNavigation } from '@react-navigation/native';
 
 import { CONSTANT } from '../themes';
@@ -33,54 +35,74 @@ const MonitorPage = () => {
       location: 'Lemah Kembar, Probolinggo',
     },
     {
-      name: 'Kolam Udang 2',
+      name: 'Kolam Udang 3',
       location: 'Lemah Kembar, Probolinggo',
     },
     {
-      name: 'Kolam Udang 2',
+      name: 'Kolam Udang 4',
       location: 'Lemah Kembar, Probolinggo',
     },
     {
-      name: 'Kolam Udang 2',
+      name: 'Kolam Udang 5',
       location: 'Lemah Kembar, Probolinggo',
     },
     {
-      name: 'Kolam Udang 2',
+      name: 'Kolam Udang 6',
       location: 'Lemah Kembar, Probolinggo',
     },
     {
-      name: 'Kolam Udang 2',
+      name: 'Kolam Udang 7',
       location: 'Lemah Kembar, Probolinggo',
     },
   ];
 
   return (
-    <SafeAreaView style={styles.homePage} className="flex-1 relative">
+    <ScrollView
+      nestedScrollEnabled={true}
+      style={styles.homePage}
+      className="flex-1 relative">
       <StatusBar />
       <View
         style={styles.headerContainer}
         className="w-full absolute rounded-b-3xl "
       />
-      <View className="mx-8 mb-4">
+      <SafeAreaView className="mx-4 px-3 mb-4 items-center">
         {/* Header Section */}
         <View className="my-10">
           <SearchComponent />
         </View>
 
         {/* Pool List Section */}
-        <FlatList
-          data={Data}
-          renderItem={({ item }) => (
-            <PoolCardComponent
-              poolName={item.name}
-              poolLocation={item.location}
+        <View>
+          <View className="flex flex-row justify-between items-center">
+            <View className="flex-row justify-center items-center space-x-1">
+              <Text style={styles.myPool}>Kolam Saya</Text>
+              <Text style={styles.separator}>●</Text>
+              {/* Change this later */}
+              <Text style={styles.count}>Count</Text>
+            </View>
+            <TouchableOpacity>
+              <Text style={styles.changePool}>Ubah</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="mt-2">
+            <FlatList
+              data={Data}
+              renderItem={({ item }) => (
+                <PoolCardComponent
+                  poolName={item.name}
+                  poolLocation={item.location}
+                />
+              )}
+              numColumns={2}
+              showsVerticalScrollIndicator={false}
+              scrollEnabled={false}
             />
-          )}
-          numColumns={2}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
-    </SafeAreaView>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -91,6 +113,25 @@ const styles = StyleSheet.create({
   headerContainer: {
     backgroundColor: CONSTANT.themeColors.primary,
     height: hp('15%'),
+  },
+  myPool: {
+    fontFamily: CONSTANT.customFonts.heading2,
+    fontSize: CONSTANT.fontSizes.heading2,
+    color: CONSTANT.themeColors.font,
+  },
+  separator: {
+    color: CONSTANT.themeColors.font,
+    fontSize: hp('1%'),
+  },
+  count: {
+    fontFamily: CONSTANT.customFonts.caption,
+    fontSize: CONSTANT.fontSizes.heading2,
+    color: CONSTANT.themeColors.font,
+  },
+  changePool: {
+    fontFamily: CONSTANT.customFonts.caption,
+    fontSize: CONSTANT.fontSizes.body,
+    color: CONSTANT.themeColors.font,
   },
 });
 
