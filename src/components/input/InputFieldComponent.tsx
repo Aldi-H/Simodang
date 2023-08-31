0b50d357ceb9886ed0b26bc342cc945608a431a9
Aldi-H <aldihimawan88@gmail.com>
2023-08-31 15:19:54 +0700

    feat: add inputFieldComponent

--- src/components/input/InputFieldComponent.tsx
+++ src/components/input/InputFieldComponent.tsx
@@ -8,21 +8,30 @@ import {
 import { CONSTANT } from '../../themes';
 
 type InputFieldProps = {
+  leftIcon: React.ReactNode;
+  rightIcon?: React.ReactNode;
   placeholder: string;
-  icon: React.ReactNode;
+  secureTextEntry: boolean;
 };
 
-const InputFieldComponent = ({ placeholder, icon }: InputFieldProps) => {
+const InputFieldComponent = ({
+  placeholder,
+  leftIcon,
+  secureTextEntry,
+  rightIcon,
+}: InputFieldProps) => {
   return (
     <View
       style={[styles.inputContainer]}
       className="flex flex-row items-center border border-gray-300 rounded-full my-2">
-      <View className="pl-4">{icon}</View>
+      <View className="pl-4">{leftIcon}</View>
       <TextInput
         style={styles.inputText}
         placeholder={placeholder}
         className="pl-2"
+        secureTextEntry={secureTextEntry}
       />
+      <View className="pr-4">{rightIcon}</View>
     </View>
   );
 };
@@ -30,12 +39,13 @@ const InputFieldComponent = ({ placeholder, icon }: InputFieldProps) => {
 const styles = StyleSheet.create({
   inputContainer: {
     height: hp('6%'),
-    width: wp('70%'),
+    width: wp('75%'),
   },
   inputText: {
     fontFamily: CONSTANT.customFonts.caption,
     fontSize: CONSTANT.fontSizes.body,
     color: CONSTANT.themeColors.disable,
+    width: wp('75%'),
   },
   onFocus: {
     borderColor: CONSTANT.themeColors.complementary,
