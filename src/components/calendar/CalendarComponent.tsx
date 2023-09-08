0c31ca3cfdf1d6570d7c38f2a1f4a5433714f4e3
Aldi-H <aldihimawan88@gmail.com>
2023-09-08 08:20:40 +0700

    style: styling calendar view

--- src/components/calendar/CalendarComponent.tsx
+++ src/components/calendar/CalendarComponent.tsx
@@ -3,15 +3,54 @@ import { View, StyleSheet } from 'react-native';
 import Calendar from 'react-native-calendar-range-picker';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
+import { CUSTOM_LOCALE } from '../../utils/calendarLocale/CustomLocal';
+import { CONSTANT } from '../../themes';
+
 const CalendarComponent = () => {
+  const today = new Date();
+  const todayFormatted = `${today.getFullYear()}-${String(
+    today.getMonth() + 1,
+  ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+
+  const sevenDaysFromToday = new Date(today);
+  sevenDaysFromToday.setDate(today.getDate() + 14);
+  const sevenDaysFromTodayFormatted = `${sevenDaysFromToday.getFullYear()}-${String(
+    sevenDaysFromToday.getMonth() + 1,
+  ).padStart(2, '0')}-${String(sevenDaysFromToday.getDate()).padStart(2, '0')}`;
+
   return (
     <View style={styles.calendarContainer}>
       <Calendar
-        startDate="2023-09-06"
-        endDate="2023-12-31"
+        // startDate="2023-09-06"
+        startDate={todayFormatted}
+        // endDate="2023-12-31"
+        endDate={sevenDaysFromTodayFormatted}
+        locale={CUSTOM_LOCALE}
         pastYearRange={0}
-        futureYearRange={2}
+        futureYearRange={1}
         isMonthFirst={true}
+        style={{
+          monthContainer: {
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          monthNameText: {
+            fontFamily: CONSTANT.customFonts.heading1,
+            fontSize: CONSTANT.fontSizes.heading2,
+            color: CONSTANT.themeColors.font,
+          },
+          dayNameText: {
+            fontFamily: CONSTANT.customFonts.heading2,
+            fontSize: CONSTANT.fontSizes.body,
+            color: CONSTANT.themeColors.font,
+          },
+          selectedDayBackgroundColor: CONSTANT.themeColors.font,
+          selectedBetweenDayBackgroundTextColor:
+            CONSTANT.themeColors.complementary,
+          dayTextColor: CONSTANT.themeColors.font,
+          todayColor: CONSTANT.themeColors.complementary,
+          selectedDayTextColor: CONSTANT.themeColors.base,
+        }}
         onChange={date => console.log(date)}
       />
     </View>
@@ -20,7 +59,8 @@ const CalendarComponent = () => {
 
 const styles = StyleSheet.create({
   calendarContainer: {
-    height: hp('80%'),
+    height: hp('70%'),
+    // paddingBottom: 24,
   },
 });
 
