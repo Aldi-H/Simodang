c7a5d4b9e79046d650021262018e2c69821253fa
Aldi-H <aldihimawan88@gmail.com>
2023-09-08 08:21:18 +0700

    chore: add custom locale for calendar component

---
+++ src/utils/calendarLocale/CustomLocal.tsx
@@ -0,0 +1,19 @@
+export const CUSTOM_LOCALE = {
+  monthNames: [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ],
+  dayNames: ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'],
+  today: '',
+  year: '',
+};
