67e643888c405a9c4930451c07ebda50fc88738d
Aldi-H <aldihimawan88@gmail.com>
2023-09-08 15:47:57 +0700

    feat: add inputField without icon

--- src/components/input/InputFieldComponent.tsx
+++ src/components/input/InputFieldComponent.tsx
@@ -1,54 +1,53 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-
 import { CONSTANT } from '../../themes';
 
 type InputFieldProps = {
-  leftIcon: React.ReactNode;
-  rightIcon?: React.ReactNode;
-  placeholder: string;
-  secureTextEntry: boolean;
+  inputTitle: string;
+  defaultValue?: string;
+  placeholder?: string;
 };
 
 const InputFieldComponent = ({
+  inputTitle,
+  defaultValue,
   placeholder,
-  leftIcon,
-  secureTextEntry,
-  rightIcon,
 }: InputFieldProps) => {
   return (
-    <View
-      style={[styles.inputContainer]}
-      className="flex flex-row items-center border border-gray-300 rounded-full my-2">
-      <View className="pl-4">{leftIcon}</View>
-      <TextInput
-        style={styles.inputText}
-        placeholder={placeholder}
-        className="pl-2"
-        secureTextEntry={secureTextEntry}
-      />
-      <View className="pr-4">{rightIcon}</View>
+    <View className="my-2">
+      <View>
+        <Text style={styles.textInputTitleStyle}>{inputTitle}</Text>
+      </View>
+      <View style={styles.inputContainer} className="rounded-md w-fit mt-1.5">
+        <TextInput
+          style={styles.textInputStyle}
+          className="w-fit pl-4"
+          placeholder={placeholder}
+          defaultValue={defaultValue}
+        />
+      </View>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
-  inputContainer: {
-    height: hp('6%'),
-    width: wp('75%'),
-  },
-  inputText: {
-    fontFamily: CONSTANT.customFonts.caption,
+  textInputTitleStyle: {
+    fontFamily: CONSTANT.customFonts.body,
     fontSize: CONSTANT.fontSizes.body,
     color: CONSTANT.themeColors.disable,
-    width: wp('75%'),
   },
-  onFocus: {
-    borderColor: CONSTANT.themeColors.complementary,
+  inputContainer: {
+    backgroundColor: CONSTANT.themeColors.complementary,
+    height: hp('7%'),
+    width: wp('80%'),
+  },
+  textInputStyle: {
+    fontFamily: CONSTANT.customFonts.heading2,
+    fontSize: CONSTANT.fontSizes.body,
+    color: CONSTANT.themeColors.font,
   },
 });
 
