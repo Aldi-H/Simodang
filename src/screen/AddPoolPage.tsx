17e1a235b908f032a1d5ba5d47bdf174a74f6070
Aldi-H <aldihimawan88@gmail.com>
2023-09-11 14:19:59 +0700

    feat: pick image and upload o server

--- src/screen/AddPoolPage.tsx
+++ src/screen/AddPoolPage.tsx
@@ -1,63 +1,157 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import {
-  KeyboardAvoidingView,
-  Platform,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
   View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Button,
 } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
-
-import BackIcon from '../assets/icons/BackIcon.svg';
-import { CONSTANT } from '../themes';
-import InputFieldComponent from '../components/input/InputFieldComponent';
+import DocumentPicker, {
+  DocumentPickerResponse,
+} from 'react-native-document-picker';
 
 const AddPoolPage = () => {
-  const navigation = useNavigation();
+  const [singleFile, setSingleFile] = useState<DocumentPickerResponse[] | null>(
+    null,
+  );
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [deviceId, setDeviceId] = useState('');
+  // const [name, setName] = useState('');
+  // const [data, setData] = useState(new FormData());
+  // const [data, setData] = useState({
+  //   name: '',
+  //   address: '',
+  //   city: '',
+  //   deviceId: '',
+  //   file: singleFile,
+  // });
+
+  const handleChangeName = (text: string) => {
+    setName(text);
+  };
+  const handleChangeAddress = (text: string) => {
+    setAddress(text);
+  };
+  const handleChangeCity = (text: string) => {
+    setCity(text);
+  };
+  const handleChangeDeviceId = (text: string) => {
+    setDeviceId(text);
+  };
+
+  const selectFile = async () => {
+    try {
+      const response = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+      });
+      console.log(response);
+
+      setSingleFile(response);
+    } catch (error) {
+      setSingleFile(null);
+      if (DocumentPicker.isCancel(error)) {
+        console.log('Canceled');
+      } else {
+        console.log('Unknown Error: ' + JSON.stringify(error));
+        throw error;
+      }
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+
+      formData.append('name', name);
+      formData.append('address', address);
+      formData.append('city', city);
+      formData.append('deviceId', deviceId);
+      formData.append('file', {
+        uri: singleFile![0].uri,
+        type: singleFile![0].type,
+        name: singleFile![0].name,
+      });
+
+      const response = await axios.post(
+        'http://www.devel-filkomub.site/ponds',
+        formData,
+        {
+          headers: {
+            Authorization:
+              'Bearer c55395c467dc5f4d8caee3d6b53c5f17d4c24b28976bcf387f1b9feb563e',
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      );
+      console.log('Response from server:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.KeyboardAvoidingView}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <ScrollView>
-        <SafeAreaView className="m-4 my-5 px-3">
-          {/* Header Title */}
-          <View className="flex flex-row items-center justify-between mt-4">
-            <Pressable
-              onPress={() => navigation.goBack()}
-              className="items-start">
-              <BackIcon fill={CONSTANT.themeColors.font} />
-            </Pressable>
-            <View className="items-center">
-              <Text style={styles.addPoolHeaderTitle}>Add Pool Page</Text>
-            </View>
-            <View />
-          </View>
+    <View>
+      <TextInput
+        placeholder="Nama"
+        onChangeText={handleChangeName}
+        value={name}
+      />
+      <TextInput
+        placeholder="Alamat"
+        onChangeText={handleChangeAddress}
+        value={address}
+      />
+      <TextInput
+        placeholder="Kota"
+        onChangeText={handleChangeCity}
+        value={city}
+      />
+      <TextInput
+        placeholder="Device Id"
+        onChangeText={handleChangeDeviceId}
+        value={deviceId}
+      />
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        activeOpacity={0.5}
+        onPress={selectFile}>
+        <Text style={styles.buttonTextStyle}>Select File</Text>
+      </TouchableOpacity>
 
-          {/* Input Section */}
-          <View className="items-center">
-            <InputFieldComponent
-              inputTitle="Nama Kolam"
-              placeholder="Nama Kolam"
-            />
-          </View>
-        </SafeAreaView>
-      </ScrollView>
-    </KeyboardAvoidingView>
+      <Button onPress={() => handleSubmit()} title="Submit" />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  KeyboardAvoidingView: {
-    flex: 1,
+  buttonStyle: {
+    backgroundColor: '#307ecc',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#307ecc',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 15,
   },
-  addPoolHeaderTitle: {
-    fontFamily: CONSTANT.customFonts.heading1,
-    fontSize: CONSTANT.fontSizes.heading1,
-    color: CONSTANT.themeColors.font,
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  textStyle: {
+    backgroundColor: '#fff',
+    fontSize: 15,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+    textAlign: 'center',
   },
 });
 
