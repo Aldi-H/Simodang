1731ec720c99be0ca7b1d24283788e7ae947ee75
Aldi-H <aldihimawan88@gmail.com>
2023-09-14 15:21:38 +0700

    chore: add scopes for google firebase sign in

--- src/types/env.d.tsx
+++ src/types/env.d.tsx
@@ -1,4 +1,6 @@
 declare module '@env' {
   export const WEBCLIENT_ID: string;
+  export const SCOPES_USERINFO_EMAIL: string;
+  export const SCOPES_USERINFO_PROFILE: string;
   export const BASE_URL: string;
 }
