16cfc40323412c85c15355dc7320a0decd005f9b
Aldi-H <aldihimawan88@gmail.com>
2023-10-05 12:03:04 +0700

    SIMD-93 add notification page

---
+++ src/screen/NotificationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const NotificationPage = () => {
+  return (
+    <View>
+      <Text>Notification Page</Text>
+    </View>
+  );
+};
+
+export default NotificationPage;
