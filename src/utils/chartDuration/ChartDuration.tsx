d4c2b340a4c7ba32666ba35866611a77004ef4f0
Aldi-H <aldihimawan88@gmail.com>
2023-10-11 20:58:53 +0700

    SIMD-74 feat: get metric data by date and show in chart and table

---
+++ src/utils/chartDuration/ChartDuration.tsx
@@ -0,0 +1,47 @@
+const ChartDurationByHour = [
+  {
+    id: 1,
+    title: '1 Jam',
+    value: 1,
+  },
+  {
+    id: 2,
+    title: '12 Jam',
+    value: 12,
+  },
+  {
+    id: 3,
+    title: '24 Jam',
+    value: 24,
+  },
+  {
+    id: 4,
+    title: '72 Jam',
+    value: 72,
+  },
+];
+
+const ChartDurationByDate = [
+  {
+    id: 1,
+    title: '7 Hari Terakhir',
+    rangeDate: 7,
+  },
+  {
+    id: 2,
+    title: '14 Hari Terakhir',
+    rangeDate: 14,
+  },
+  {
+    id: 3,
+    title: '30 Hari Terakhir',
+    rangeDate: 30,
+  },
+  {
+    id: 4,
+    title: '90 Hari Terakhir',
+    rangeDate: 90,
+  },
+];
+
+export { ChartDurationByHour, ChartDurationByDate };
