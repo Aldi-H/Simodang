4e409046a89d06335d63673f43ee04e5e14ded8d
Aldi-H <aldihimawan88@gmail.com>
2023-10-11 10:16:25 +0700

    SIMD-94 feat: show notification data and apply lazyload

--- src/screen/NotificationPage.tsx
+++ src/screen/NotificationPage.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect, useState, useCallback } from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
+import { RefreshControl, ScrollView } from 'react-native-gesture-handler';
+
+import useNotificationStore from '../store/notification/NotificationStore';
 
 import { CONSTANT } from '../themes';
 
@@ -14,13 +24,40 @@ import DeleteIconOutline from '../assets/icons/DeleteIconOutline.svg';
 import NotificationCardComponent from '../components/cards/NotificationCardComponent';
 
 const NotificationPage = () => {
+  const {
+    getAllNotification,
+    notificationData,
+    updateNotificationStatus,
+    loadMoreData,
+    isLoading,
+    visibleData,
+  } = useNotificationStore();
+
+  const [refreshing, setRefreshing] = useState(false);
+
   const navigation = useNavigation();
 
+  useEffect(() => {
+    getAllNotification();
+  }, []);
+
+  const onHandleIsRead = (notifId: string) => {
+    updateNotificationStatus(notifId);
+  };
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    if (!isLoading) {
+      await getAllNotification();
+    }
+    setRefreshing(false);
+  }, [getAllNotification, isLoading]);
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <SafeAreaView className="m-4 my-5 px-3">
+    <ScrollView>
+      <SafeAreaView className="my-3">
         {/* Header Title */}
-        <View className="flex flex-row items-center justify-between mt-4">
+        <View className="flex flex-row items-center justify-between my-3 mx-4 px-3">
           <Pressable
             className="items-start"
             onPress={() => navigation.goBack()}>
@@ -36,15 +73,57 @@ const NotificationPage = () => {
               height={hp('3.5%')}
               width={wp('5%')}
               fill={CONSTANT.themeColors.warningRed}
-              onPress={() => console.log('Delete All Pressed')}
+              onPress={() => console.log('Deleted')}
             />
           </Pressable>
         </View>
 
-        <View className="mt-10">
-          <NotificationCardComponent />
-          <NotificationCardComponent />
+        <View className="mt-3">
+          <FlatList
+            scrollEnabled={false}
+            ItemSeparatorComponent={() => {
+              return (
+                <View
+                  className="px-3 mx-4 opacity-20"
+                  style={{
+                    borderColor: CONSTANT.themeColors.disable,
+                    borderWidth: StyleSheet.hairlineWidth,
+                  }}
+                />
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            data={visibleData}
+            renderItem={({ item }) => {
+              return (
+                <NotificationCardComponent
+                  containerStyle={
+                    item.isRead
+                      ? styles.notificationContainerStyleReaded
+                      : styles.notificationContainerStyleUnread
+                  }
+                  notificationTitle={item.title}
+                  notificationMessage={item.message}
+                  onPress={() => {
+                    console.log('Pressed', item.notifId);
+                    onHandleIsRead(item.notifId);
+                    getAllNotification();
+                  }}
+                />
+              );
+            }}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+          />
         </View>
+        {visibleData.length < notificationData.length && (
+          <Button
+            title="Tampilkan Lebih Banyak"
+            onPress={loadMoreData}
+            disabled={visibleData.length >= notificationData.length}
+          />
+        )}
       </SafeAreaView>
     </ScrollView>
   );
@@ -55,6 +134,12 @@ const styles = StyleSheet.create({
     fontFamily: CONSTANT.customFonts.heading1,
     fontSize: CONSTANT.fontSizes.heading1,
     color: CONSTANT.themeColors.font,
+  },
+  notificationContainerStyleReaded: {
+    backgroundColor: CONSTANT.themeColors.base,
+  },
+  notificationContainerStyleUnread: {
+    backgroundColor: CONSTANT.themeColors.complementary,
   },
 });
 
---
+++ src/store/notification/NotificationStore.ts
@@ -0,0 +1,113 @@
+import { BASE_URL } from '@env';
+import axios from 'axios';
+import { create } from 'zustand';
+import useAuthStore from '../auth/AuthStore';
+
+type NotificationData = {
+  notifId: string;
+  title: string;
+  message: string;
+  isRead: boolean;
+  createdAt: string;
+};
+
+type notificationStoreState = {
+  notificationData: NotificationData[];
+  visibleData: NotificationData[];
+  isLoading: boolean;
+  hasMore: boolean;
+  limit: number;
+};
+
+type notificationAction = {
+  getAllNotification: () => Promise<void>;
+  updateNotificationStatus: (notifId: string) => Promise<void>;
+  loadMoreData: () => void;
+};
+
+const useNotificationStore = create<
+  notificationStoreState & notificationAction
+>()((set, get) => ({
+  notificationData: [],
+  visibleData: [],
+  isLoading: false,
+  hasMore: true,
+  limit: 5,
+
+  getAllNotification: async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/notifications`, {
+        headers: {
+          Authorization: `Bearer ${useAuthStore.getState().userDetail.token}`,
+        },
+      });
+
+      const notificationData = response.data.map(
+        (dataItem: {
+          id: string;
+          title: string;
+          message: string;
+          isRead: boolean;
+          createdAt: string;
+        }) => {
+          return {
+            notifId: dataItem.id,
+            title: dataItem.title,
+            message: dataItem.message,
+            isRead: dataItem.isRead,
+            createdAt: dataItem.createdAt,
+          };
+        },
+      );
+
+      set({
+        notificationData: notificationData,
+        visibleData: notificationData.slice(0, get().limit),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    set({ isLoading: false });
+  },
+
+  updateNotificationStatus: async (notifId: string) => {
+    try {
+      const response = await axios.patch(
+        `${BASE_URL}/notifications/${notifId}`,
+        notifId,
+        {
+          headers: {
+            Authorization: `Bearer ${useAuthStore.getState().userDetail.token}`,
+          },
+        },
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  loadMoreData: () => {
+    try {
+      const currentlyVisibleCount = get().visibleData.length;
+
+      const remainingData = get().notificationData.slice(
+        currentlyVisibleCount,
+        currentlyVisibleCount + get().limit,
+      );
+
+      if (remainingData.length > 0) {
+        set(prevData => ({
+          ...prevData,
+          visibleData: [...prevData.visibleData, ...remainingData],
+        }));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
+
+export default useNotificationStore;
